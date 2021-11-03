1e95bbb57a214becf22dd81824d81fc4440b8eb3
Dino200013 <81235983+Dino200013@users.noreply.github.com>
2021-11-03 16:25:37 +0800

    ASP.NET期末專題
    可以對寶可夢圖鑑進行基本的新增、編輯和修改的動作，事前必須進行註冊以及登入，並有驗證的步驟。

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using prj1.Models;
+using System.Web.Security;
+using PagedList;
+
+namespace prj1.Controllers
+{
+    public class HomeController : Controller
+    {
+        Entities db = new Entities();
+        int pageSize = 5; //設定一頁顯示5筆資料
+        // GET: Home
+        public ActionResult Index(int page = 1)
+        {
+            int currentPage = page < 1 ? 1 : page; //若page小於1則顯示第一頁
+            var pokemons = db.Tablepokemons1081640.ToList();
+            var result = pokemons.ToPagedList(currentPage, pageSize);
+            return View(result);
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string fUserId, string fPwd)
+        {
+
+            var member = db.TableMembers1081640 //把註冊好的帳密做比對
+                .Where(m => m.fUserId == fUserId && m.fPwd == fPwd)
+                .FirstOrDefault();
+
+            if (member == null) //未註冊狀況
+            {
+                ViewBag.Message = "帳號或密碼錯誤";
+                return View();
+            }
+
+            Session["Welcome"] = member.fName + "歡迎光臨"; //顯示登入成功的訊息
+            FormsAuthentication.RedirectFromLoginPage(fUserId, true);
+            return RedirectToAction("Index", "Member");
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(TableMembers1081640 aMember)
+        {
+            if (ModelState.IsValid == false) //沒有通過驗證直接顯示目前頁面
+            {
+                return View();
+            }
+
+            var member = db.TableMembers1081640 //取得已註冊的會員帳號
+                .Where(m => m.fUserId == aMember.fUserId)
+                .FirstOrDefault();
+
+            if (member == null) //還未註冊狀況
+            {
+                db.TableMembers1081640.Add(aMember); //新增會員
+                db.SaveChanges();
+                return RedirectToAction("Login");
+            }
+            ViewBag.Message = "帳號重複，註冊失敗";
+            return View();
+        }
+
+
+
+
+
+        
+
+        
+    }
+}
---
+++ Controllers/MemberController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using prj1.Models;
+using System.Web.Security;
+using PagedList;
+
+namespace prj1.Controllers
+{
+    [Authorize]
+    public class MemberController : Controller
+    {
+        Entities db = new Entities();
+        // GET: Member
+        int pageSize = 5; //設定一頁顯示5筆資料
+        public ActionResult Index(int page = 1)
+        {
+            int currentPage = page < 1 ? 1 : page;
+            var pokemons = db.Tablepokemons1081640 //把所有寶可夢放進pokemons建立List
+                .OrderBy(m => m.pId).ToList();
+            var result = pokemons.ToPagedList(currentPage, pageSize);
+            return View("../Home/Index", "_LayoutMember", result); //使用_LayoutMember頁面
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();   // 登出
+            return RedirectToAction("Login", "Home");
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Tablepokemons1081640 pokemon)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Error = false;
+                var temp = db.Tablepokemons1081640.Where(m => m.pId == pokemon.pId)
+                    .FirstOrDefault();
+                if (temp != null)
+                {
+                    ViewBag.Error = true;
+                    return View(pokemon);
+                }
+                db.Tablepokemons1081640.Add(pokemon);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(pokemon);
+        }
+
+        public ActionResult Edit(string pId)
+        {
+            var employee = db.Tablepokemons1081640
+                .Where(m => m.pId == pId).FirstOrDefault();
+            return View(employee);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Tablepokemons1081640 pokemon)
+        {
+            if (ModelState.IsValid)
+            {
+                var temp = db.Tablepokemons1081640
+                    .Where(m => m.pId == pokemon.pId)
+                    .FirstOrDefault();
+                temp.pName = pokemon.pName;
+                temp.pType = pokemon.pType;
+                temp.pLv = pokemon.pLv;
+                temp.pHp = pokemon.pHp;
+                temp.pAtk = pokemon.pAtk;
+                temp.pDef = pokemon.pDef;
+                temp.pSpeed = pokemon.pSpeed;
+                temp.pMoves = pokemon.pMoves;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(pokemon);
+        }
+
+        public ActionResult Delete(string pId)
+        {
+            var pokemon = db.Tablepokemons1081640
+                .Where(m => m.pId == pId).FirstOrDefault();
+            db.Tablepokemons1081640.Remove(pokemon);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
